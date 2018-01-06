d9a39994806827518b808aed1a3954b34bb2b1c3
Christopher Diep <diep.christopher@gmail.com>
2018-01-06 16:07:35 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -2,5 +2,5 @@ require "pry"
 
 def alphabetize(arr)
   Esperanto_alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
-  organized = arr.sort_by! {|Esperanto_alphabet| w}
+  organized = arr.sort_by! {|w| w}
 end
