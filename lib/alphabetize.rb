9a53f2c18c4075bf8bbf0914682b9249225eb871
Christopher Diep <diep.christopher@gmail.com>
2018-01-06 16:24:05 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -2,5 +2,6 @@ require "pry"
 
 def alphabetize(arr)
   esp_alphabet_arr = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz".chars
-  organized = arr.sort_by! {|w| w}
+  arr.sort_by do |w|
+    w.chars.map {|c| esp_alphabet_arr.index(c)}
 end
