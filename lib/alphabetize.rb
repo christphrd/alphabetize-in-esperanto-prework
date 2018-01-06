3d8047f8d9d2a2a2d9db57b850d09d8f846ac43d
Christopher Diep <diep.christopher@gmail.com>
2018-01-06 15:58:45 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -2,6 +2,6 @@ require "pry"
 
 def alphabetize(arr)
   binding.pry
-  ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
-  arr.sort_by {|w| ESPERANTO_ALPHABET}
+  "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  arr.sort_by {|w| "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"}
 end
