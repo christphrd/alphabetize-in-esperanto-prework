a93674984f28d33309dda7f63703798cd0dada89
Christopher Diep <diep.christopher@gmail.com>
2018-01-06 16:28:49 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -6,6 +6,7 @@ def alphabetize(arr)
 
   #take the arr sort by (each element by this rule)
   arr.sort_by do |w|
+    binding.pry
     #rule = create char array and get your index number from the alphabet
     w.chars.map {|c| esp_alphabet_arr.index(c)}
   end
