3c53d038fcce23cbbcfc11d14a51d2424f12b19c
Christopher Diep <diep.christopher@gmail.com>
2018-01-06 16:28:37 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,8 +1,12 @@
 require "pry"
 
 def alphabetize(arr)
+  #create array of each char in the string
   esp_alphabet_arr = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz".chars
+
+  #take the arr sort by (each element by this rule)
   arr.sort_by do |w|
+    #rule = create char array and get your index number from the alphabet
     w.chars.map {|c| esp_alphabet_arr.index(c)}
   end
 end
